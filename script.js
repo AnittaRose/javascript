a6861bba2629b2ac2deeaa70c32145a0db250f0f
anittanelson <anittanelson40@gmail.com>
2024-07-23 11:28:40 +0530

    sample

---
+++ script.js
@@ -0,0 +1,42 @@
+let str='';
+for (let i=0; i<5; i++){
+    for (let j=5; j>i; j--){
+        str=str+"*";
+    }
+    str=str+"\n";
+}
+console.log(str)
+
+
+console.log("\n\n\n\n\n")
+let x='';
+for (let i=0; i<=5; i++){
+    for(let j=1; j<=i; j++){
+        x=x+j;
+    }
+    x=x+"\n";
+}
+console.log(x)
+
+console.log("\n\n\n\n\n")
+for(let i=1; i<=10; i++)
+    console.log(i)
+
+console.log("\n\n\n\n\n")
+let sum=0;
+for(let i=1; i<=10; i++){
+    sum=sum+i
+}
+    console.log(sum)
+
+
+console.log("\n\n\n\n\n")
+
+for(let i=0; i<=15; i++){
+    if (i % 2==0){
+        console.log(i);
+    }
+}
+
+
+
