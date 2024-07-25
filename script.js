b2b95f732a2d9b80bae00af1caabecf6f5fff16d
anittanelson <anittanelson40@gmail.com>
2024-07-25 11:18:44 +0530

    third

--- script.js
+++ script.js
@@ -40,3 +40,107 @@ for(let i=0; i<=15; i++){
 
 
 
+
+
+
+
+console.log("\n\n\n\n\n")
+//timer functions
+
+
+   setTimeout (function (){
+    console.log ("this is from  setTimeout");
+   },5000);
+
+
+   function putZero(value){
+    return value <10 ? "0"+ value: value;
+   }
+
+
+function show() {
+    let dt = new Date();
+    console.log ("dt: ",dt);
+
+
+    let hours = dt .getHours();
+    console.log ("hours ",hours);
+
+
+    let ampm = hours <12 ? "am" :"pm";
+    console.log ("ampm ",ampm);
+
+
+
+    let time =document.getElementById ('time');
+    console.log ("time ",time);
+
+
+    time.innerHTML = putZero(hours) + ":" + putZero (dt.getMinutes()) + ":" +
+    putZero(dt.getSeconds())
+
+    // setTimeout (show,1000);
+}       
+show()
+
+console.log("\n\n\n\n\n")
+//event handling
+
+
+let btn= document.getElementById('btn');
+console.log ("btn ",btn);
+
+
+
+btn.addEventListener ('click' ,function (){
+    console.log("Button clicked.....");
+});
+
+btn.addEventListener('mouseover' ,function (){
+    console.log("mouseover event fired....");
+});
+
+btn.addEventListener('mouseout' ,function (){
+    console.log("mouseout event fired....");
+});
+
+btn.addEventListener('mousedown' ,function (){
+    console.log("mousedown event fired....");
+});
+
+btn.addEventListener('mouseup' ,function (){
+    console.log("mouseup event fired....");
+});
+
+
+btn.addEventListener('mousemove' ,function (){
+    console.log("mousemove event fired....");
+});
+
+let inp = document.getElementById('text')
+console.log("inp : ",inp);
+
+inp.addEventListener('keydown',function (){
+    console.log("keydown event fired.....");
+})
+
+
+inp.addEventListener('keyup',function (){
+    console.log("keyup event fired.....");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
